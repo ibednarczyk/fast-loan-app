6e8a2db1631b51f155f24cd63900c22722ac936e
Izabela Bednarczyk <izabela.bednarczyk93@gmail.com>
2020-05-10 15:42:58 +0200

    Attachment of REST API test collection using Postman

---
+++ src/test/java/com/loan_application/LoanApplicationTestSuite.java
@@ -0,0 +1,13 @@
+package com.loan_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LoanApplicationTestSuite {
+
+    @Test
+    public void contextLoads(){
+
+    }
+}
