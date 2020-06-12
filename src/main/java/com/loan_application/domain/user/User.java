53afad9eb94279333a93e0227aa314dcfddb7b3b
Izabela Bednarczyk <izabela.bednarczyk93@gmail.com>
2020-06-12 14:22:47 +0200

    User entity- change for @Builder annotation, adding fields

--- src/main/java/com/loan_application/domain/user/User.java
+++ src/main/java/com/loan_application/domain/user/User.java
@@ -15,6 +15,7 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
+@Builder
 @Table(name = "USERS")
 public class User {
 
@@ -23,6 +24,12 @@ public class User {
     @GeneratedValue
     @Column(name = "ID", unique = true)
     private Long userId;
+
+    @Column(name = "USERNAME", nullable = false)
+    private String userName;
+
+    @Column(name = "PASSWORD", nullable = false)
+    private String password;
 
     @Column(name = "FIRST_NAME")
     private String firstName;
@@ -35,6 +42,12 @@ public class User {
 
     @Column(name = "NUMBER")
     private String phoneNumber;
+
+    @Column(name = "ROLE")
+    private String role;
+
+    @Column(name = "ACTIVE")
+    private boolean active;
 
 
     @JsonManagedReference
@@ -55,58 +68,5 @@ public class User {
     )
     private List<Loan> loansList;
 
-    private User(Builder builder){
-        this.firstName = builder.firstName;
-        this.lastName = builder.lastName;
-        this.email = builder.email;
-        this.phoneNumber = builder.phoneNumber;
-        this.applicationsList = builder.applicationsList;
-        this.loansList = builder.loansList;
 
-    }
-    public static class Builder{
-        private String firstName;
-        private String lastName;
-        private String email;
-        private String phoneNumber;
-        private List<Application> applicationsList;
-        private List<Loan> loansList;
-
-
-
-        public Builder firstName(String firstName){
-            this.firstName = firstName;
-            return this;
-
-        }
-        public Builder lastName(String lastName){
-            this.lastName = lastName;
-            return this;
-
-        }
-        public Builder email(String email){
-            this.email = email;
-            return this;
-
-        }
-        public Builder phoneNumber(String phoneNumber){
-            this.phoneNumber = phoneNumber;
-            return this;
-
-        }
-        public Builder applicationsList(List<Application> applicationsList){
-            this.applicationsList= applicationsList;
-            return this;
-
-        }
-        public Builder loansList(List<Loan> loansList){
-            this.loansList = loansList;
-            return this;
-
-        }
-        public User build(){
-            return new User(this);
-        }
-
-    }
 }
