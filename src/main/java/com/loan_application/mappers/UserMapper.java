ada1ff4ea3fa8fb46cd70e07aecab691b75c36f9
Izabela Bednarczyk <izabela.bednarczyk93@gmail.com>
2020-07-13 13:30:43 +0200

    Using Orika Mapping library

--- src/main/java/com/loan_application/mappers/UserMapper.java
+++ src/main/java/com/loan_application/mappers/UserMapper.java
@@ -2,6 +2,8 @@ package com.loan_application.mappers;
 
 import com.loan_application.domain.user.User;
 import com.loan_application.representation.UserDto;
+import ma.glasnost.orika.MapperFacade;
+import ma.glasnost.orika.impl.DefaultMapperFactory;
 
 public class UserMapper {
 
@@ -9,30 +11,13 @@ public class UserMapper {
     }
 
     public static UserDto mapToUserDto(User user) {
-        return UserDto.builder()
-                .userId(user.getUserId())
-                .userName(user.getUserName())
-                .password(user.getPassword())
-                .firstName(user.getFirstName())
-                .lastName(user.getLastName())
-                .email(user.getEmail())
-                .phoneNumber(user.getPhoneNumber())
-                .role(user.getRole())
-                .active(user.isActive())
-                .build();
+        MapperFacade mapper = new DefaultMapperFactory.Builder().build().getMapperFacade();
+        return mapper.map(user, UserDto.class);
     }
 
     public static User mapToUser(UserDto userDto) {
-        return User.builder()
-                .userName(userDto.getUserName())
-                .password(userDto.getPassword())
-                .firstName(userDto.getFirstName())
-                .lastName(userDto.getLastName())
-                .email(userDto.getEmail())
-                .phoneNumber(userDto.getPhoneNumber())
-                .role(userDto.getRole())
-                .active(userDto.isActive())
-                .build();
+        MapperFacade mapperFacade = new DefaultMapperFactory.Builder().build().getMapperFacade();
+        return mapperFacade.map(userDto, User.class);
     }
 
 }
